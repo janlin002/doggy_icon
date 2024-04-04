d21ecc736bb3ced0c2de2ddace7f51fada61c034
janlin002 <123davidbill@gmail.com>
2024-04-04 11:19:02 +0800

    feat: add icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,28 +1,45 @@
 import search from "./search.svg";
-import heart from './heart.svg'
-import heart_outline from './heart_outline.svg'
-import supervise from './supervise.svg'
-import backtest from './backtest.svg'
-import stop_loss from './stop_loss.svg'
-import stop_profit from './stop_profit.svg'
-import holding_time from './holding_time.svg'
-import automatic_updates from './automatic_updates.svg'
-import performance from './performance.svg'
-import copy from './copy.svg'
-import share from './share.svg'
-import editing from './editing.svg'
-import delete_icon from './delete.svg'
-import new_folder from './new_folder.svg'
-import folder from './folder.svg'
-import add from './add.svg'
-import funnels from './funnels.svg'
-import more from './more.svg'
-import card_mode from './card_mode.svg'
-import table_mode from './table_mode.svg'
-import info from './info.svg'
-import notifications from './notifications.svg'
-import magnifier from './magnifier.svg'
-import cross from './cross.svg'
+import heart from "./heart.svg";
+import heart_outline from "./heart_outline.svg";
+import supervise from "./supervise.svg";
+import backtest from "./backtest.svg";
+import stop_loss from "./stop_loss.svg";
+import stop_profit from "./stop_profit.svg";
+import holding_time from "./holding_time.svg";
+import automatic_updates from "./automatic_updates.svg";
+import performance from "./performance.svg";
+import copy from "./copy.svg";
+import share from "./share.svg";
+import editing from "./editing.svg";
+import delete_icon from "./delete.svg";
+import new_folder from "./new_folder.svg";
+import folder from "./folder.svg";
+import add from "./add.svg";
+import funnels from "./funnels.svg";
+import more from "./more.svg";
+import card_mode from "./card_mode.svg";
+import table_mode from "./table_mode.svg";
+import info from "./info.svg";
+import notifications from "./notifications.svg";
+import magnifier from "./magnifier.svg";
+import cross from "./cross.svg";
+import setting from "./setting.svg";
+import mail from "./mail.svg";
+import system from "./system.svg";
+import upload from "./upload.svg";
+import popula_strategies from "./popula_strategies.svg";
+import newest_on_the_shelf from "./newest_on_the_shelf.svg";
+import more_info from "./more_info.svg";
+import caret_left from "./caret_left.svg";
+import caret_right from "./caret_right.svg";
+import caret_down from "./caret_down.svg";
+import caret_up from "./caret_up.svg";
+import chevron_right from "./chevron_right.svg";
+import chevron_left from "./chevron_left.svg";
+import chevron_down from "./chevron_down.svg";
+import chevron_up from "./chevron_up.svg";
+import ascending_order from "./ascending_order.svg";
+import descending_order from "./descending_order.svg";
 
 const Icon: { [key: string]: string } = {
   search,
@@ -49,7 +66,24 @@ const Icon: { [key: string]: string } = {
   info,
   notifications,
   magnifier,
-  cross
+  cross,
+  setting,
+  mail,
+  system,
+  upload,
+  popula_strategies,
+  newest_on_the_shelf,
+  more_info,
+  caret_left,
+  caret_right,
+  caret_down,
+  caret_up,
+  chevron_right,
+  chevron_left,
+  chevron_down,
+  chevron_up,
+  ascending_order,
+  descending_order,
 };
 
 export default Icon;
